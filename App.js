baf237dcc7066676ebe98aa33ed217df33080cf5
UsamaAsif3 <191370237@gift.edu.pk>
2023-08-26 09:12:31 +0500

    Updated Home Screen with name and rollNo

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Name: Usama Asif 
+        Roll No: 191370237
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
